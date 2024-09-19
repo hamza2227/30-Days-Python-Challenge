cafcc91c0c645acd8d45a91b7c77ce98e3d7f8fd
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-19 12:36:58 +0500

    Checking Data Types & Casting

--- 02-Day-variables-buitin-functions/variables-buitin-functions.py
+++ 02-Day-variables-buitin-functions/variables-buitin-functions.py
@@ -1,5 +1,6 @@
 
 # Variables in Python
+
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
 country = 'Finland'
@@ -52,10 +53,58 @@ print('Age: ', age)
 print('Married: ', is_married)
 
 
+# Checking Data types and Casting
+
+first_name = 'Asabeneh'     # str
+last_name = 'Yetayeh'       # str
+country = 'Finland'         # str
+city= 'Helsinki'            # str
+age = 250                   # int, it is not my real age, don't worry about it
+
+# Printing out types
+print(type('Asabeneh'))     # str
+print(type(first_name))     # str
+print(type(10))             # int
+print(type(3.14))           # float
+print(type(1 + 1j))         # complex
+print(type(True))           # bool
+print(type([1, 2, 3, 4]))     # list
+print(type({'name':'Asabeneh','age':250, 'is_married':250}))    # dict
+print(type((1,2)))                                              # tuple
+print(type(zip([1,2],[3,4])))                                   # set
+
+# Casting : Converting one data type to another data type.
+
+# int to float
+num_int = 10
+print("int = ", num_int)          # 10
+num_float = float(num_int)
+print("float = ", num_float)      # 10.0
+
+# float to int
+gravity = 9.81
+print(int(gravity))                # 9
+
+# int to str
+num_int = 10
+num_str = str(num_int)
+print(num_str)                     # '10'
+
+# str to int or float
+num_str = '10.6'
+print('num_int', int(num_str))      # 10
+print('num_float', float(num_str))  # 10.6
+
+# str to list
+
+first_name = "Hamza"
+print(first_name)         # Hamza
+print(list(first_name))   # ['H', 'a', 'm', 'z', 'a']
+
 # Getting user input using the input() built-in function
 
-first_name = input('What is your name: ')
-age = input('How old are you? ')
+# first_name = input('What is your name: ')
+# age = input('How old are you? ')
 
 print(first_name)
 print(age)
