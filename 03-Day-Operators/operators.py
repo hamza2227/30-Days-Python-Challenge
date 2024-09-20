43eed500abb5be0a0971d97731622af40b88e30b
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-20 23:26:43 +0500

    Update operators.py

--- 03-Day-Operators/operators.py
+++ 03-Day-Operators/operators.py
@@ -123,3 +123,28 @@ print('coding' in 'coding for all')           # True - because coding for all ha
 print('a in an = ', 'a' in 'an')              # True
 print('4 is 2 ** 2 = ', 4 is 2 ** 2)          # True
 
+
+# Logical Operators
+# Unlike other programming languages python uses keywords and, or and not for logical operators. 
+# Logical operators are used to combine conditional statements.
+
+# AND ...
+print(3 > 2 and 4 > 3) # True - because both statements are true
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print('True and True =  ', True and True)
+
+# OR ...
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statements is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print('True or False = ', True or False)
+
+# NOT ...
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
+
+
