0091f97f0caf622737efc9bc1bcd4f93d3363375
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-20 23:20:41 +0500

    Update operators.py

--- 03-Day-Operators/operators.py
+++ 03-Day-Operators/operators.py
@@ -60,7 +60,6 @@ print('a % b = ', remainder)
 print('a // b = ', floor_division)
 print('a ** b = ', exponential)
 
-
 # Example ...
 
 # Calculating area of a circle
@@ -84,4 +83,43 @@ print("Weight of Object = ",weight)
 mass = 75 
 volume = 0.075 
 density = mass / volume 
+
+
+# Comparison Operators
+
+print(3 > 2)     # True, because 3 is greater than 2
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False,  because 3 is greater than 2
+print(2 < 3)     # True, because 2 is less than 3
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))  # False
+print(len('mango') != len('avocado'))  # True
+print(len('mango') < len('avocado'))   # True
+print(len('milk') != len('meat'))      # False
+print(len('milk') == len('meat'))      # True
+print(len('tomato') == len('potato'))  # True
+print(len('python') > len('dragon'))   # False
+
+# Comparing something gives either a True or False
+
+print('True == True: ', True == True)    # True
+print('True == False: ', True == False)  # False
+print('False == False:', False == False) # True
+
+# In addition to the above comparison operator Python uses:
+
+# is: Returns true if both variables are the same object(x is y)
+# is not: Returns true if both variables are not the same object(x is not y)
+# in: Returns True if the queried list contains a certain item(x in y)
+# not in: Returns True if the queried list doesn't have a certain item(x in y)
+
+print("1 is 1 = ", 1 is 2)                    # True - because the data values are the same
+print('1 is not 2 = ', 1 is not 2)            # True - because 1 is not 2
+print('A in Asabeneh = ', 'A' in 'Asabeneh')  # True - A found in the string
+print('B in Asabeneh = ', 'B' in 'Asabeneh')  # False - there is no uppercase B
+print('coding' in 'coding for all')           # True - because coding for all has the word coding
+print('a in an = ', 'a' in 'an')              # True
+print('4 is 2 ** 2 = ', 4 is 2 ** 2)          # True
 
