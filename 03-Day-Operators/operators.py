47ea985740738446bd7f089b510e5a34e0a29078
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-20 23:00:25 +0500

    03-Day-Operators

---
+++ 03-Day-Operators/operators.py
@@ -0,0 +1,66 @@
+## Boolean
+# A boolean data type represents one of the two values: True or False.
+
+print(True)
+print(False)
+
+## Operators
+# Python language supports several types of operators. In this section, we will focus on few of them.
+
+# Assignment Operators:
+# Assignment operators are used to assign values to variables. Let us take = as an example.
+
+
+# Arithmetic Operators:
+# Addition(+): a + b
+# Subtraction(-): a - b
+# Multiplication(*): a * b
+# Division(/): a / b
+# Modulus(%): a % b
+# Floor division(//): a // b
+# Exponentiation(**): a ** b
+
+# Integers
+print('Addition: ', 1 + 2)        # 3
+print('Subtraction: ', 2 - 1)     # 1
+print('Multiplication: ', 2 * 3)  # 6
+print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+print('Division: ', 6 / 2)        # 3.0         
+print('Division: ', 7 / 2)        # 3.5
+print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+print ('Division without the remainder: ',7 // 3)   # 2
+print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+
+# Floats
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+
+# Example ...
+a = 3
+b = 2
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+print(total) 
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
+
+
+
+
+
