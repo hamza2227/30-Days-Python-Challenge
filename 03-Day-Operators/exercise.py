c01b812837e621822668b50131115efdfcc3edb9
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-20 23:57:07 +0500

    03-Day exercise

---
+++ 03-Day-Operators/exercise.py
@@ -0,0 +1,41 @@
+
+# 1. Write a script that prompts the user to enter base 
+# and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
+# base = float(input("Enter base = "))
+# height = float(input("Enter height = "))
+# area_of_triangle = 0.5 * base * height
+# print("Area of Triangle = ",area_of_triangle)
+
+
+# 2. Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
+# Calculate the perimeter of the triangle (perimeter = a + b + c).
+
+# side_a = float(input("Enter side a = "))
+# side_b = float(input("Enter side b = "))
+# side_c = float(input("Enter side c = "))
+# perimeter_of_triangle = side_a + side_b + side_c
+# print("Perimeter of Traingle = ",perimeter_of_triangle)
+
+
+# 3. Get length and width of a rectangle using prompt. 
+# Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+
+# length = float(input("Enter lenght = "))
+# width = float(input("Enter width = "))
+# area_of_rectangle = length * width
+# print("Area of Rectangle = ",area_of_rectangle)
+
+# perimeter_of_rectangle = 2 * (length + width)
+# print("Perimeter of Rectangle = ",perimeter_of_rectangle)
+
+
+# 4. Get radius of a circle using prompt. 
+# Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+
+radius = float(input("Enter radius of circle = "))
+area_of_circle = 3.14 * radius * radius
+print("Area of circle = ",area_of_circle)
+
+circumference_of_circle = 2 * 3.14 * radius
+print("Circumference of circle = ",circumference_of_circle)
