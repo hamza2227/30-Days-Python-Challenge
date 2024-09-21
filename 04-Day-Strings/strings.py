700c9dc663eb7cf66d132fa8e47c758ef1ffbe23
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-21 23:26:53 +0500

    04-Day-Strings

---
+++ 04-Day-Strings/strings.py
@@ -0,0 +1,76 @@
+# String : Text is a string data type. Any data type written as text is a string.
+
+
+# Creating a String
+
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be made using a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of Python Challenge"
+print(sentence)
+
+
+# Multiline string is created by using triple single (''') or triple double quotes (""").
+
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
+
+# Another way of doing the same thing
+multiline_string = """I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python."""
+print(multiline_string)
+
+
+# String Concatenation
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Asabeneh Yetayeh
+# Checking the length of a string using len() built-in function
+print(len(first_name))  # 8
+print(len(last_name))   # 7
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 16
+
+
+# Escape Sequences in Strings
+
+# In Python and other programming languages \ followed by a character is an escape sequence. Let us see the most common escape characters:
+
+# \n: new line
+# \t: Tab means(8 spaces)
+# \\: Back slash
+# \': Single quote (')
+# \": Double quote (")
+
+print('I hope everyone is enjoying the Python Challenge.\nAre you ?') # line break
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces 
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+print('This is a backslash  symbol (\\)') # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
+
+
+# String Interpolation / f-Strings (Python 3.6+)
+# Another new string formatting is string interpolation, f-strings. 
+# Strings start with f and we can inject the data in their corresponding positions.
+
+a = 4
+b = 3
+print(f'{a} + {b} = {a +b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
