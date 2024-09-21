db36c3e1757353716718c169f7c7ea8a5cc964b2
Hamza Altaf <hamzaaltaf298@gmail.com>
2024-09-21 22:46:32 +0500

    03 Day exercise

--- 03-Day-Operators/exercise.py
+++ 03-Day-Operators/exercise.py
@@ -1,6 +1,6 @@
 
-# 1. Write a script that prompts the user to enter base 
-# and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+# # 1. Write a script that prompts the user to enter base 
+# # and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
 
 # base = float(input("Enter base = "))
 # height = float(input("Enter height = "))
@@ -8,8 +8,8 @@
 # print("Area of Triangle = ",area_of_triangle)
 
 
-# 2. Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
-# Calculate the perimeter of the triangle (perimeter = a + b + c).
+# # 2. Write a script that prompts the user to enter side a, side b, and side c of the triangle. 
+# # Calculate the perimeter of the triangle (perimeter = a + b + c).
 
 # side_a = float(input("Enter side a = "))
 # side_b = float(input("Enter side b = "))
@@ -18,8 +18,8 @@
 # print("Perimeter of Traingle = ",perimeter_of_triangle)
 
 
-# 3. Get length and width of a rectangle using prompt. 
-# Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+# # 3. Get length and width of a rectangle using prompt. 
+# # Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
 
 # length = float(input("Enter lenght = "))
 # width = float(input("Enter width = "))
@@ -30,12 +30,25 @@
 # print("Perimeter of Rectangle = ",perimeter_of_rectangle)
 
 
-# 4. Get radius of a circle using prompt. 
-# Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+# # 4. Get radius of a circle using prompt. 
+# # Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
 
-radius = float(input("Enter radius of circle = "))
-area_of_circle = 3.14 * radius * radius
-print("Area of circle = ",area_of_circle)
+# radius = float(input("Enter radius of circle = "))
+# area_of_circle = 3.14 * radius * radius
+# print("Area of circle = ",area_of_circle)
 
-circumference_of_circle = 2 * 3.14 * radius
-print("Circumference of circle = ",circumference_of_circle)
+# circumference_of_circle = 2 * 3.14 * radius
+# print("Circumference of circle = ",circumference_of_circle)
+
+
+# # 5. Calculate the slope of line.
+
+# x1 = float(input("Enter value of x1 = "))
+# y1 = float(input("Enter value of y1 = "))
+# x2 = float(input("Enter value of x2 = "))
+# y2 = float(input("Enter value of y2 = "))
+# slope = (y2-y1)/(x2-x1)
+# print("Slope of line = ",slope)
+
+
+
