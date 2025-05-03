551aedf1d5701f0cbf5b2d9d57d45d9c0cfa8ad9
Hamza Altaf <hamzaaltaf298@gmail.com>
2025-05-04 00:02:44 +0500

    indentation

--- 01-Day-Introduction/hello-world.py
+++ 01-Day-Introduction/hello-world.py
@@ -1,36 +1,7 @@
 
-
-# print("Hello World")
-
-# print(2 + 3)   # addition(+)
-# print(3 - 1)   # subtraction(-)
-# print(2 * 3)   # multiplication(*)
-# print(3 / 2)   # division(/)
-# print(3 ** 2)  # exponential(**)
-# print(3 % 2)   # modulus(%)
-# print(3 // 2)  # Floor division operator(//) 
-
-
-# # Checking data types
-
-# print(type(10))                  # Int
-# print(type(3.14))                # Float
-# print(type(1 + 3j))              # Complex
-# print(type('Asabeneh'))          # String
-# print(type([1, 2, 3]))           # List
-# print(type({'name':'Asabeneh'})) # Dictionary
-# print(type({9.8, 3.14, 2.7}))    # Set
-# print(type((9.8, 3.14, 2.7)))    # Tuple
-
-
-
 print("Hello World")
 
 
-print(10 + 2)      # addition (+)
-print(10 - 2)      # subtraction (-)
-print(10 / 2)      # division (/)
-print(10 * 2)      # multiplication (*)
-print(10 ** 2)     # exponential (**)
-print(10 % 2)      # modules (%)
-print(10 // 3)     # floor division operator (//)
+# print is a built in function that tells the computer to show something on screen.
+# "Hello World" is a string. which means its just a text. In python string always written in quotes single ' '
+# or double " ".
---
+++ 01-Day-Introduction/indentationn.py
@@ -0,0 +1,14 @@
+# In python indentation is defined as block of code. It tells python that this group of statment belongs to a
+# specific block. 
+
+if 10 > 5:
+    print("This is true!")
+    print("I am tab indentation")
+
+print("I have no indentation")
+
+
+
+# -- The first two print statements are indented by 4 spaces, so they belong to if block.
+# -- The third print is not indented so it is outside the if block.
+
