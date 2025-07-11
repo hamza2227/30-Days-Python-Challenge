f824899496da5f7b3cddc04bf43f5fffcff600eb
Hamza Altaf <hamzaaltaf298@gmail.com>
2025-07-12 01:18:33 +0500

    number guessing game

--- Python_Projects/number_guessing_game.py
+++ Python_Projects/number_guessing_game.py
@@ -1,6 +1,16 @@
-import random
-
+import random    
 compNum = random.randint(1, 100)
 
-userNum = input("Guess a number between 1 and 100")
-
+while True:
+    try:
+        userNum = int(input("Guess a number between 1 and 100 : "))
+    except ValueError:
+        print("Please enter a valid number")
+        
+    if userNum > compNum:
+        print("Too High!")
+    elif userNum < compNum:
+        print("Too Low!")
+    else:
+        print("Congratulations... You Guess Right!")
+        break
